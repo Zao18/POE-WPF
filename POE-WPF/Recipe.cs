75f6e7387f444279e56feab02a222a283521344f
Zao18 <126759508+Zao18@users.noreply.github.com>
2024-06-26 12:43:41 +0200

    Add project files.

---
+++ POE-WPF/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_WPF
+{
+    internal class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<Steps> Steps { get; set; }
+
+        public Recipe(string name)
+        {
+            Name = name;
+            Ingredients = new List<Ingredient>();
+            Steps = new List<Steps>();
+        }
+    }
+}
+
